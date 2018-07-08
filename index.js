6bd4454ba39fa8100fd46d1d91a2168e69e4ae2d
Michael Boroski <mjboroski@gmail.com>
2018-07-08 16:35:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-function driversWithRevenueOver(list, revenue){
-  
+function driversWithRevenueOver(list, rev){
+  list.filter(function(driver){
+    return driver.revenue > rev
+  })
 }
 
 function driverNamesWithRevenueOver(list, revenue){
-  
+
 }
 
 function exactMatch(list, revenue){
-  
+
 }
 
 function exactMatchToList(list, revenue){
-  
-}
 
+}
